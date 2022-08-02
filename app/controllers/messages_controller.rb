f16777d5081860c3aae2845c8f067052d31c242b
yeemans <arthur.roth.07@gmail.com>
2022-08-02 16:59:12 -0700

    removed second turbo stream

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,10 +2,6 @@ class MessagesController < ApplicationController
   def create
     @current_user = current_user
     @message = @current_user.messages.create(content: msg_params[:content], room_id: params[:room_id])
-    #respond_to do |format|
-     # format.turbo_stream
-      #format.html{ redirect_to message_path(params[:message][:id])}
-    #end
   end
 
   private
