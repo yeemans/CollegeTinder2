db9f0a5a5160f6e5f1fcb63fb02f0125c98978e4
bob <arthur.roth.07@gmail.com>
2022-08-04 22:26:30 -0700

    updated profile layout

--- app/javascript/collegeForm.js
+++ app/javascript/collegeForm.js
@@ -1,5 +1,7 @@
-let button = document.querySelector("#change_college_info"); 
+let button = document.querySelector("#change_college_info")
 button.addEventListener("click", () => {
   let form = document.querySelector("#college_form"); 
+  let caption = document.querySelector("caption");
   form.classList.toggle("hidden");
+  caption.classList.toggle("hidden");
 } )
